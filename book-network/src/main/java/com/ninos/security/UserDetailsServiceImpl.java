ff5b9c0bcdb635251bc6c4943bdeb9dc12db1a1f
NinoosMoshi <ninos1357@yahoo.com>
2024-05-10 09:41:05 -0500

    add to github

---
+++ book-network/src/main/java/com/ninos/security/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.ninos.security;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ninos.user.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+     return userRepository.findByEmail(userEmail)
+             .orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
+    }
+}
